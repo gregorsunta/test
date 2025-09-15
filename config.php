634f4ba0b7ee7788da6d5eb57955717721a92ff2
GitHub Action <action@github.com>
2025-09-15 11:26:39 +0000

    Auto-bump asset versions [skip ci]

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ class DBoxConfig {
 
     const WEBSITE_VERSION = '7.2.5'; // Used for rollbar to track website version.
 
-    const WEBSITE_CSS_VERSION = 10;
+    const WEBSITE_CSS_VERSION = 11;
 
     const WEBSITE_JS_VERSION = 2;
 
